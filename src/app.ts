4225acb8507334df7b5d1eb2f2021f8c1a103939
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-09-14 00:54:59 +0600

    Initialized user module

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { userRoutes } from "./app/modules/user/user";
 
 const app: Application = express();
 app.use(cors());
+app.use("/api/v1/user", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
