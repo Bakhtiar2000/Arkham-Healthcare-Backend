3eac26f9f980db87896ef7b431346ccbfd3d7967
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-09-14 01:17:01 +0600

    Initialized basic structure of user

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { userRoutes } from "./app/modules/user/user";
+import { userRoutes } from "./app/modules/user/user.route";
 
 const app: Application = express();
 app.use(cors());
+
+// Parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/v1/user", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,9 @@
+const createAdmin = async () => {
+  return {
+    message: "User Created",
+  };
+};
+
+export const userServices = {
+  createAdmin,
+};
