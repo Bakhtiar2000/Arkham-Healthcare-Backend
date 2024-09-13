6684115e2de7aa84180a07763c7cd9e21d43e748
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-09-14 00:41:12 +0600

    added user schema

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
+import { Server } from "http";
 import app from "./app";
 
 const port = 3000;
-app.listen(port, () => {
-  console.log("App is listening at port ", port);
-});
+
+async function main() {
+  const server: Server = app.listen(port, () => {
+    console.log("Server is running on ", port);
+  });
+}
+
+main();
