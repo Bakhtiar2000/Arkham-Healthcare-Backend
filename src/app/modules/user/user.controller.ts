a1fb5444574c45c99abc92ac8e76afd9192c2776
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-09-15 18:06:09 +0600

    added hashed Password and created admin

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,8 +2,7 @@ import { Request, Response } from "express";
 import { userServices } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  console.log(req.body);
-  const result = await userServices.createAdmin();
+  const result = await userServices.createAdmin(req.body);
   res.send(result);
 };
 
