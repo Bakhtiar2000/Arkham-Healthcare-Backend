0928811dd75d05c757b4254e11a077f80dd11202
Bakhtiar <bakhtiarfahim360@gmail.com>
2025-04-07 10:53:05 +0600

    updated authorization

--- src/app/modules/schedule/schedule.route.ts
+++ src/app/modules/schedule/schedule.route.ts
@@ -5,7 +5,11 @@ import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
-router.get("/", auth(UserRole.DOCTOR), scheduleControllers.getAllSchedules);
+router.get(
+  "/",
+  auth(UserRole.DOCTOR, UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  scheduleControllers.getAllSchedules
+);
 
 router.post("/", scheduleControllers.createSchedule);
 
